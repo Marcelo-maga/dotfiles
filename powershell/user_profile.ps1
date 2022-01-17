09f4e1bbfbbbaa70b2966ab980a5be68b0179a36
Marcelo <marcelo.maga2550@gmail.com>
2022-01-17 00:08:18 -0300

    First Commit

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,16 @@
+# set PowerShell to UTF-8
+
+Import-Module posh-git
+Import-Module oh-my-posh
+Import-Module -Name Terminal-Icons
+
+Set-poshPrompt Paradox
+
+$omp_config = Join-Path $PSScriptRoot ".\marcelo.omp.json"
+oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+Set-Alias -Name vim -Value nvim
